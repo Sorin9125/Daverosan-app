175be6547a649e7d681c96a953a84011fca1cc9f
Sorin Ioan <sorinmariusioan1@gmail.com>
2025-07-25 15:04:03 +0300

    Incarcat fisiere excel -> nu merge trebuie reparat

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,5 +1,6 @@
 const {Sequelize} = require("sequelize");
-const env = require("dotenv");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: "localhost",
--- back-end/server.js
+++ back-end/server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const { db } = require("./models");
 const routes = require("./routes");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 app.use(express.json());
+
 const port = 1234;
 
 app.get("/", async (req, res) => {
@@ -15,6 +18,7 @@ app.post("/reset", async (req, res) => {
         res.status(200).send("Database reset succeeded");
     } catch (err) {
         console.log(err);
+        res.status(500).send("eroare");
     }
 });
 
