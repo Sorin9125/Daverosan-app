1baddbd7d3e430bd0f1ceff220c42051f0f9fbf0
Sorin Ioan <sorinmariusioan1@gmail.com>
2025-07-30 15:23:19 +0300

    Cookie monster type sht

--- back-end/server.js
+++ back-end/server.js
@@ -3,8 +3,29 @@ const express = require("express");
 const { db } = require("./models");
 const routes = require("./routes");
 const app = express()
+const cors = require("cors");
 app.use(express.json());
 dotenv.config();
+
+app.use(cors({
+    origin: [process.env.ORIGIN_SITE],
+    credentials: true,
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "Acces-Control-Allow-Methods",
+        "Acces-Control-Request-Headers",
+        "Acces-Control-Allow-Origin"
+    ],
+    methods: [
+        "GET",
+        "POST",
+        "PUT",
+        "PATCH",
+        "DELETE",
+    ]
+})
+);
 
 const port = process.env.PORT;
 app.get("/", async (req, res) => {
@@ -17,7 +38,7 @@ app.post("/reset", async (req, res) => {
         res.status(200).send("Database reset succeeded");
     } catch (err) {
         console.log(err);
-        res.status(500).send("eroare");
+        res.status(500).send("Eroare!");
     }
 });
 
